b336dc45f9634007b8ba6dfe35aab825fa88ef60
MaidouPP <leept416@gmail.com>
2021-06-03 23:29:26 -0700

    Add dependencies and some fixes for ai_food

--- ai_food/run_env_prep.sh
+++ ai_food/run_env_prep.sh
@@ -20,13 +20,17 @@ apt-get -y install libc-dev
 apt-get --yes install python3.7
 apt-get --yes install python3-pip
 python3 -m pip install --upgrade pip requests
-pip install ipython==7.5.0
-pip install notebook
+pip install pandas ipython==7.5.0 notebook matplotlib plotly pystan==2.19.1.1
 
 # Set the timezone.
 apt-get install tzdata
 ln -snf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
 echo "America/Los_Angeles" > /etc/timezone
+
+# Fix locale issue.
+apt-get --yes install locales
+locale-gen en_US en_US.UTF-8
+dpkg-reconfigure locales
 
 unset DEBIAN_FRONTEND
 rm -rf /var/lib/apt/lists/*
